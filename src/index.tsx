e74c2b34b0db668e8dfb30fa9c484984305c2695
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-16 00:38:04 +0500

    removed AddTask import

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import AddTask from "./components/AddTask";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
