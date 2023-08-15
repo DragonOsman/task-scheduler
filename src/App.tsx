7e78e66279fff11931df9de1b0a99ecd073468ea
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-16 00:05:13 +0500

    deleted most rules from App.css, added Scheduler code and routing functionality to App.tsx, and added AddTask and BrowserRouter to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, ComponentType } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Scheduler, DayView, Appointments } from "@devexpress/dx-react-scheduler-material-ui";
+import {
+  EditRecurrenceMenu,
+  EditingState,
+  ChangeSet
+} from "@devexpress/dx-react-scheduler";
+import AddTask from "./components/AddTask";
+
+interface ITask {
+  id: number;
+  title: string;
+  startDate: Date;
+  endDate: Date;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const changeHandler = ({ added, changed, deleted }: ChangeSet) => {
+    if (added) {
+      setTasks([...tasks, added as ITask])
+    } else if (changed) {
+      setTasks(tasks.map(task => {
+        return changed[task.id] ? { ...task, ...changed[task.id] } : task
+      }));
+    } else if (deleted !== undefined) {
+      setTasks(tasks.filter(task => task.id !== deleted));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Scheduler data={tasks}>
+        <DayView startDayHour={9} endDayHour={19} />
+        <EditingState onCommitChanges={changeHandler} />
+        <Appointments />
+        <EditRecurrenceMenu
+          layoutComponent={
+            EditRecurrenceMenu.defaultProps?.layoutComponent as ComponentType<EditRecurrenceMenu.LayoutProps>
+          }
+          overlayComponent={
+            EditRecurrenceMenu.defaultProps?.overlayComponent as ComponentType<EditRecurrenceMenu.OverlayProps>
+          }
+          buttonComponent={
+            EditRecurrenceMenu.defaultProps?.buttonComponent as ComponentType<EditRecurrenceMenu.ButtonProps>
+          }
+        />
+      </Scheduler>
+      <Routes>
+        <Route path="/add-task" element={<AddTask />} />
+      </Routes>
     </div>
   );
 }
