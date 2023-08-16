2e0cf70253c9f57e763f1a9747cafe6594eecaa8
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-16 05:24:06 +0500

    went back to useState from useContext & trying to keep scheduler code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 import "./App.css";
-import { useState, ComponentType } from "react";
-import { Routes, Route } from "react-router-dom";
+import { ComponentType, useState } from "react";
 import { Scheduler, DayView, Appointments } from "@devexpress/dx-react-scheduler-material-ui";
 import {
   EditRecurrenceMenu,
   EditingState,
   ChangeSet
 } from "@devexpress/dx-react-scheduler";
-import AddTask from "./components/AddTask";
 
 interface ITask {
   id: number;
   title: string;
   startDate: Date;
-  endDate: Date;
+  endDate: Date
 }
 
 function App() {
-  const [tasks, setTasks] = useState<ITask[]>([])
+  const [tasks, setTasks] = useState<ITask[]>([]);
 
   const changeHandler = ({ added, changed, deleted }: ChangeSet) => {
     if (added) {
-      setTasks([...tasks, added as ITask])
+      const newTaskId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 0;
+      setTasks([...tasks, { ...added as ITask, id: newTaskId }]);
     } else if (changed) {
-      setTasks(tasks.map(task => {
-        return changed[task.id] ? { ...task, ...changed[task.id] } : task
+      setTasks(tasks.map((task: ITask): ITask => {
+        return changed[task.id] ? { ...task, ...changed[task.id] } : task;
       }));
     } else if (deleted !== undefined) {
       setTasks(tasks.filter(task => task.id !== deleted));
     }
+    return { tasks };
   };
 
   return (
@@ -49,9 +49,6 @@ function App() {
           }
         />
       </Scheduler>
-      <Routes>
-        <Route path="/add-task" element={<AddTask />} />
-      </Routes>
     </div>
   );
 }
