1ffef17cc13ef0bb02e25150e6b9fc5a9a574965
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-21 02:05:02 +0500

    nested App component inside TaskContext Provider and BrowserRouter for routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import TaskContext from "./context/TaskContext";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <TaskContext>
+        <App />
+      </TaskContext>
+    </Router>
   </React.StrictMode>
 );
 
