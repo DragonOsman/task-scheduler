91e5466f8fffee5e391efd94455ca3d532186d73
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-21 02:41:02 +0500

    removed all imports and code from Home.tsx except for importing and rendering ListTasks

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,6 @@
-import { useState, useEffect } from "react";
 import ListTasks from "./ListTasks";
 
-const Home = () => {
-  const [currentChild, setCurrentChild] = useState("Amira");
-
-  const toggleCurrentChild = (child: string) => setCurrentChild(child);
-
-  useEffect(() => {
-    if (currentChild === "Amira") {
-      toggleCurrentChild("Noora");
-    } else if (currentChild === "Noora") {
-      toggleCurrentChild("Amira");
-    }
-  }, [currentChild]);
-
-  return (
+const Home = () => {return (
     <div className="container-fluid">
       <ListTasks />
     </div>
