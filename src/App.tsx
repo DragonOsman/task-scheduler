0867ff6e3bc3db4f2cdd0f380a3c508348c5d419
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-22 01:17:57 +0500

    added Header component in App.tsx to render it there and removed it from Home.tsx, and removed display-1 class from h1 in Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import Home from "./components/Home";
+import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Home />} path="/" />
-      <Route element={<AddTask />} path="/add-task" />
-    </Routes>
+    <>
+      <Header/>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<AddTask />} path="/add-task" />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="d-flex justify-content-center py-3">
-      <h1 className="display-1">Task Scheduler</h1>
+      <h1>Task Scheduler</h1>
     </header>
   );
 };
