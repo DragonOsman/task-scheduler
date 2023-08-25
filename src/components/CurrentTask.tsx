2daea84d871dfd5480cfc974a8dffbe5594f7aca
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-25 19:27:38 +0500

    fixed code problems and added progress bar for task timer

--- src/components/CurrentTask.tsx
+++ src/components/CurrentTask.tsx
@@ -1,11 +1,11 @@
 import { useTaskContext } from "../context/taskContext";
 import { useTimer } from "react-timer-hook";
-import { Link } from "react-router-dom";
 import addPadding from "../addPadding";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 interface TaskTimerProps {
   expiryTimestamp: Date;
-};
+}
 
 const TaskTimer = ({ expiryTimestamp }: TaskTimerProps) => {
   const {
@@ -13,56 +13,70 @@ const TaskTimer = ({ expiryTimestamp }: TaskTimerProps) => {
     minutes,
     hours,
     days
-  } = useTimer({ expiryTimestamp, onExpire: () => <p>Time is up!</p> });
+  } = useTimer({ expiryTimestamp, onExpire: () => console.log("Time is up!") });
 
-  const daysStr = days.toString();
-  const hoursStr = hours.toString();
-  const minutesStr = minutes.toString();
-  const secondsStr = seconds.toString();
+  // getting amount of time elapsed as a percentage by getting and using the total
+  // num of seconds
+  const totalSeconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
+  const progressPercentage = (totalSeconds / (expiryTimestamp.getTime() / 1000)) * 100;
+
+  const daysStr = days.toString().length === 1 ?
+    addPadding(days.toString()) :
+    days.toString()
+  ;
+  const hoursStr = hours.toString().length === 1 ?
+    addPadding(hours.toString()) :
+    hours.toString()
+  ;
+  const minutesStr = minutes.toString().length === 1 ?
+    addPadding(minutes.toString()) :
+    minutes.toString()
+  ;
+  const secondsStr = seconds.toString().length === 1 ?
+    addPadding(seconds.toString()) :
+    seconds.toString()
+  ;
 
   return (
     <div className="task-timer">
-      <span>{daysStr.length === 1 ? addPadding(daysStr) : daysStr}</span>:
-      <span>{hoursStr.length === 1 ? addPadding(hoursStr) : hoursStr}</span>:
-      <span>{minutesStr.length === 1 ? addPadding(minutesStr) : minutesStr}</span>:
-      <span>{secondsStr.length === 1 ? addPadding(secondsStr) : secondsStr}</span>
+      <ProgressBar completed={progressPercentage} bgColor="blue" animateOnRender={true} />
+      <p className="percentage">Task timer progress percentage: {progressPercentage}%</p>
+      <span>{daysStr}</span>:
+      <span>{hoursStr}</span>:
+      <span>{minutesStr}</span>:
+      <span>{secondsStr}</span>
       <br />
     </div>
   );
 };
 
 const CurrentTask = () => {
-  const [state, dispatch] = useTaskContext();
-  let nextTwoTasks: typeof state.tasks = [];
+  const [{ currentTask, tasks }] = useTaskContext();
+  let nextTwoTasks: typeof tasks = [];
 
-  if (state.currentTask) {
-    const startIndex = state.tasks.indexOf(state.currentTask);
-    nextTwoTasks = state.tasks.slice(startIndex + 1, startIndex + 3);
+  if (currentTask) {
+    const startIndex = tasks.indexOf(currentTask);
+    nextTwoTasks = tasks.slice(startIndex + 1, startIndex + 3);
   }
 
   return (
     <div className="task-details">
       <div className="current-task">
-        {state.currentTask && (
+        {currentTask && (
           <>
-            <h2 className="task-title">Title: {state.currentTask.title}</h2>
+            <h2 className="task-title">Title: {currentTask.title}</h2>
             <p className="task-is-recurring">
-              Recurring: {state.currentTask.isRecurring ? "Yes" : "No"}
+              Recurring: {currentTask.isRecurring ? "Yes" : "No"}
             </p>
-            <TaskTimer expiryTimestamp={state.currentTask.endTime} />
+            <TaskTimer expiryTimestamp={currentTask.endTime} />
             Days recurring:
-            {state.currentTask.isRecurring && (
+            {currentTask.isRecurring && (
               <ul className="days-recurring">
-                {state.currentTask.daysRecurring.map(day => {
-                  return (
-                    <li key={day}>
-                      {`${state.currentTask
-                        && state.currentTask.daysRecurring[
-                          state.currentTask.daysRecurring.length - 1]
-                           === day ? `${day}` : `${day}, `}`}
-                    </li>
-                  );
-                })}
+                {currentTask.daysRecurring.map((day, index) => (
+                  <li key={day}>
+                    {`${day}${index !== currentTask.daysRecurring.length - 1 ? ", " : ""}`}
+                  </li>
+                ))}
               </ul>
             )}
           </>
@@ -70,24 +84,17 @@ const CurrentTask = () => {
       </div>
       <div className="next-two-tasks">
         <p>
-          {nextTwoTasks && (
-            nextTwoTasks.length > 1 ? "Your next two tasks:" :
-              nextTwoTasks.length === 1 ? "Your next task:" : ""
-          )}
+          {nextTwoTasks.length > 0 &&
+            `Your next ${nextTwoTasks.length === 1 ? "task:" : "two tasks:"}`}
         </p>
         <ul className="tasks">
-          {nextTwoTasks && (
-            nextTwoTasks.map(task => {
-              return (
-                <li key={task.id}>
-                  {task.title}
-                </li>
-              );
-            })
-          )}
+          {nextTwoTasks.map(task => (
+            <li key={task.id}>
+              {task.title}
+            </li>
+          ))}
         </ul>
       </div>
-      <Link to="/" className="back-to-home btn btn-primary">Back to Home</Link>
     </div>
   );
 };
--- src/context/taskContext.tsx
+++ src/context/taskContext.tsx
@@ -64,19 +64,17 @@ const tasksReducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "ADD_TASK":
       return { tasks: action.payload.tasks,
-        task: action.payload?.task, currentChild: action.payload?.currentChild
+        task: action.payload?.task
       };
     case "EDIT_TASK":
       return {
         task: action.payload?.task,
         tasks: action.payload.tasks.map(task => task.id === action.payload.task?.id ?
-          action.payload.task : task),
-        currentChild: action.payload?.currentChild
+          action.payload.task : task)
       };
     case "DELETE_TASK":
       return {
-        tasks: action.payload.tasks.filter(task => task.id !== action.payload.task?.id),
-        currentChild: action.payload?.currentChild
+        tasks: action.payload.tasks.filter(task => task.id !== action.payload.task?.id)
       };
     case "SELECT_CURRENT_TASK":
       return {
@@ -101,18 +99,30 @@ const TaskProvider = ({ children }: TaskProviderProps) => {
   useEffect(() => {
     dispatch({ type: "SELECT_CURRENT_TASK", payload: {
       tasks: state.tasks,
-      currentTask: state.tasks.find(task => !task.isCompleted)
+      currentTask: state.tasks.find(task => !task.isCompleted &&
+        new Date(task.endTime) > new Date())
     } });
   }, [state.tasks]);
 
   // fill tasks array
   useEffect(() => {
     dispatch({ type: "ADD_TASK", payload: {
-      tasks: tasks.map(task => ({
-        ...task,
-        startTime: new Date(task.startTime),
-        endTime: new Date(task.endTime)
-      }))
+      tasks: tasks.map(task => {
+        const startTimeStr = task.startTime;
+        const startTimeArr = startTimeStr.split(":");
+        const startTime = new Date();
+        startTime.setHours(Number(startTimeArr[0]));
+        startTime.setMinutes(Number(startTimeArr[1]));
+        startTime.setSeconds(Number(startTimeArr[2]));
+        const endTimeStr = task.endTime;
+        const endTimeArr = endTimeStr.split(":");
+        const endTime = new Date();
+        endTime.setHours(Number(endTimeArr[0]));
+        endTime.setMinutes(Number(endTimeArr[1]));
+        endTime.setSeconds(Number(endTimeArr[2]));
+
+        return { ...task, startTime, endTime };
+      })
     } });
   }, []);
 
