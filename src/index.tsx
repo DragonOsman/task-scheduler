9818cfa3b53715d3a8bc04b53df6ab596eef0dda
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-28 18:11:51 +0500

    changed import linefor TaskContextProvider to take moved file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import TaskContextProvider from "./context/taskContext";
+import TaskContextProvider from "../../context/taskContext";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
