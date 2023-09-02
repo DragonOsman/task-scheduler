a1bfa0d909eef56109f834ba0017184c998fac2d
Osman Zakir <osmanzakir90@hotmail.com>
2023-09-02 21:19:59 +0500

    brought files from cra build over

---
+++ app/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./components/Home";
+import AddTask from "./components/AddTask";
+import { NextRouter, useRouter} from "next/router";
+import React from "react";
+
+function App() {
+  const router = useRouter();
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
