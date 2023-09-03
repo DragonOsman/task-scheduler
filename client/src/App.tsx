6e42aa99c9317e6f0eaac5cd8f774eeb2bfb831f
Osman Zakir <osmanzakir90@hotmail.com>
2023-09-03 16:52:43 +0500

    starting move to vite from cra

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Home from "./components/Home";
+import AddTask from "./components/AddTask";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<AddTask />} path="/add-task" />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
